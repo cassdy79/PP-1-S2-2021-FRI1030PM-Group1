5500a1b653a77e6e6b4459cb6d9778a71b9245e8
Cass Ilangantileke <s3742928@student.rmit.edu.au>
2021-08-15 16:01:12 +1000

    added database creation check

--- model/database.php
+++ model/database.php
@@ -6,4 +6,20 @@ if (mysqli_connect_errno()) {
     printf(mysqli_connect_error());
     exit();
 }
+
+$check = mysqli_query($db, 'select 1 from `users` LIMIT 1');
+
+if($check === FALSE)
+{
+    $query = "CREATE TABLE IF NOT EXISTS `users` (
+        `id` int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        `fullname` varchar(100) NOT NULL,
+        `phone` varchar(100) NOT NULL,
+        `email` varchar(100) NOT NULL,
+        `password` varchar(100) NOT NULL
+      )";
+
+     mysqli_query($db, $query);
+}
+
 include("users.php");
