1057e36b54c6759693a91d63e5019327e5472090
vellam1 <michaelv_360@hotmail.com>
2021-08-31 14:44:45 +1000

    edited map to dark mode

--- view/home.php
+++ view/home.php
@@ -3,7 +3,11 @@
 ?>
 
 <div class="header">
+	<?php if(isset($_SESSION['email'])) : ?>
+		<h2>Welcome, <?= $user['fullname'] ?></h2>
+	<?php else : ?>
 	<h2>Home Page</h2>
+	<?php endif; ?>
 	<img class="homeLogo";
 	src="/view/images/logo.png" alt="TSB Logo">
 
