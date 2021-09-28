1109ac7c6bddc8dbbdc61f7e3fdf803a49cd5d38
vellam1 <michaelv_360@hotmail.com>
2021-09-28 14:17:59 +1000

    initial work done

--- view/map.php
+++ view/map.php
@@ -22,9 +22,6 @@ if ($currentBooking) {
 }
 ?>
 
-
-
-
 <div class="mapHeader">
 	<h2>Map</h2>
 </div>
@@ -35,14 +32,45 @@ if ($currentBooking) {
 <?php endif; ?>
 
 <div class="map_container">
-        
-<div id="map">
 
-        </div>
-        </div>
+  <div id="map">
+
+  </div> </div>
+
+</div>
+
+<div class="closestHeader">
+  <h2>Closest Available Spots</h2>
+</div>
+
+<div class="closestContent">
+  <button onclick="getLocation()"  id="locationButtons" class="btn">Find closest locations</button>
+  <p id="closest">
+    fuck fuck shit fuck
+  </p>
+</div>
+
 
 </div>
 <script defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBzdTq4OTYoUN274uw-QC2pHGy52MVNyOo&callback=initMap">
+</script>
+
+<script>
+
+var loc = document.getElementById("closest");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  }
+   else {
+  loc.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+function showPosition(position) {
+  loc.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
 </script>
 <?php include('layouts/footer.php') ?>
